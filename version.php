3537a24bbb80d8c701c135ae45c16d05927a4509
fellowapeman <box@up.edu>
2013-07-22 15:13:44 -0700

    Update version.php

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 $plugin->cron = 60;
 $plugin->version = 2013061200;
-$plugin->requires = 2012061700;
+$plugin->requires = 2012062500;
 $plugin->component = 'assignsubmission_mahara';
 
 $plugin->dependencies = array(
