324889c9c53fe94b69f4bc9a6378a84c9ef755c2
fellowapeman <box@up.edu>
2013-09-17 11:36:40 -0700

    Update version.php

--- version.php
+++ version.php
@@ -3,6 +3,8 @@
 $plugin->cron = 60;
 $plugin->version = 2013061200;
 $plugin->requires = 2012062500;
+$plugin->release = '1.0rc (Build: 2013061200)';
+$plugin->maturity = MATURITY_RC;
 $plugin->component = 'assignsubmission_mahara';
 
 $plugin->dependencies = array(
